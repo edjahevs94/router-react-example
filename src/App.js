5394dd6d05e00cc1c4c8998a8ec6110099027d51
Edgard <20133252@ealoe.ulima.edu.pe>
2022-09-27 17:55:38 -0500

    div

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
         <nav className="navbar navbar-expand navbar-light bg-light">
           <div className="container-fluid">
             <div className='nav navbar-nav'>
-              <div className="nav-item nav-link active" > <a class="nav-link" href="#">Home</a></div>
-              <div className="nav-item nav-link" ><a class="nav-link" href="#">Create project</a></div>
+              <div className="nav-item nav-link"> Home </div>
+              <div className="nav-item nav-link" >Create project</div>
             </div>
           </div>
         </nav>
